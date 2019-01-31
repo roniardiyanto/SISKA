da8c91d0258f847e5387e670c53cf04db542ad05
roniardiyanto <roni.ardiyanto@koding.sch.id>
2019-01-31 20:40:50 +0700

    Versi Pertama Awal Login Page

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistem Informasi Keuangan</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cabin+Condensed" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Archivo|Carme|Gudea|Jockey+One|Staatliches" rel="stylesheet"> 
+</head>
+<body>
+    <main>
+        <div class="atas"><h1>Sistem Informasi Keuangan</h1></div>
+        <div id="halamanMasuk"><h2>Halaman Masuk</h2></div>
+        <form action="login" method="post">
+            <div class="inputForm">
+                    <input type="text" placeholder="Nama Pengguna" value="">
+            </div>
+            <div class="inputForm">
+                    <input type="password" placeholder="Kata Sandi" value="">
+            </div>
+            <input class="btn" type="submit" name="" value="MASUK">
+
+        </form>
+    </main>
+</body>
